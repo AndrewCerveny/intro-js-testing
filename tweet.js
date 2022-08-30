df844a670653ab7656da268eb6ebad03c4002bec
Andrew Cerveny <andrewfcerveny0628@gmail.com>
2022-08-30 17:09:36 -0400

    Added completed tweet test practice form

--- tweet.js
+++ tweet.js
@@ -1,4 +1,25 @@
 class Tweet {
+  constructor(handle,content,link,likes){
+    this.sender = handle
+    this.content = content
+    this.link = link || 'no link provided'
+    this.likes = 0
+    this.retweets = 0
+    this.replies = []
+    this.replyCount = 0
+  }
+  like() {
+   this.likes += 1
+  }
+
+  retweet() {
+    this.retweets += 1
+  }
+
+  reply(message) {
+  this.replies.push(message)
+  this.replyCount ++
+  }
 
 }
 
